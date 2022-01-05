4281115c0eb2f99ede5bfba71433cade876d6970
Jerome Escultura <esculturajerome@gmail.com>
2022-01-06 03:02:19 +0800

    Finalized forms

--- components/Footer.js
+++ components/Footer.js
@@ -1,7 +1,7 @@
 export default function Footer() {
   return (
     <>
-      <footer className="py-10 px-5  bg-mainColor text-white">
+      <footer className="py-4 px-5  bg-mainColor text-white">
         &copy; 2022 Juno Web Designs. All rights reserved.
       </footer>
     </>
--- components/GetInTouch.js
+++ components/GetInTouch.js
@@ -10,29 +10,72 @@ function GetInTouch() {
       >
         <div className="">
           <p className="text-2xl md:text-3xl lg:text-4xl lg:font-semibold text-mainTextColor">
-            Let us hear from you
+            Get in touch
           </p>
           <p className="md:text-lg mt-3 text-secondaryTextColor">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Aut eius
-            earum magnam nulla mollitia molestiae ad impedit ducimus rerum ut,
-            atque reiciendis asperiores! Eaque obcaecati deleniti in assumenda!
-            Officiis, molestiae.
+            Want to know more us? <br /> We're ready to answer any and all
+            questions.
           </p>
         </div>
         <div className="w-full max-w-lg py-10 px-6 bg-white rounded-lg hover:bg-gray-50 shadow-xl shadow-shadowColor">
-          <form className="px-8 pt-6 pb-8 mb-4" data-netlify="true">
+          <form
+            className="px-8 pt-6 pb-8 mb-4"
+            data-netlify="true"
+            method="post"
+            name="Contact Form"
+            onSubmit="submit"
+            data-netlify-honeypot="bot-field"
+          >
+            <input type="hidden" name="contact-form" value="Contact Form" />
+            <div hidden>
+              <input name="bot-field" />
+            </div>
             <div className="mb-4">
               <label
                 className="block text-gray-700 text-sm font-bold mb-2"
                 for="username"
               >
-                Name
+                Name*
               </label>
               <input
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
                 id="username"
                 type="text"
                 placeholder="Name"
+                required
+                name="Name"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="email"
+                htmlFor="email"
+              >
+                Email
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="email"
+                type="email"
+                placeholder="Email"
+                name="Email"
+              />
+            </div>
+            <div className="mb-4">
+              <label
+                className="block text-gray-700 text-sm font-bold mb-2"
+                for="number"
+                htmlFor="number"
+              >
+                Phone Number
+              </label>
+              <input
+                className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                id="number"
+                type="text"
+                placeholder="Phone Number"
+                name="Number"
               />
             </div>
             <div className="mb-6">
@@ -40,17 +83,16 @@ function GetInTouch() {
                 className="block text-gray-700 text-sm font-bold mb-2"
                 for="message"
               >
-                Message
+                Message*
               </label>
               <textarea
                 className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
                 id="message"
                 type="text"
+                name="Message"
+                required
                 placeholder="Send us a message"
               />
-              {/* <p className="text-red-500 text-xs italic">
-              Please choose a password.
-            </p> */}
             </div>
             <div className="flex items-center justify-between">
               <button
